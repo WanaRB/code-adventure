841787f5acba6ec16cc44b5d96719a860af5e01a
WanaRB <159739459+WanaRB@users.noreply.github.com>
2026-04-27 10:00:53 +0700

    tt

--- builds/web/index.service.worker.js
+++ builds/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777258742|5587445026';
+const CACHE_VERSION = '1777258838|5683957846';
 /** @type {string} */
 const CACHE_PREFIX = 'Code_Adventure-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
